41303dd5d0bc240f63f121c5a57e721c02ac9194
Fernando Duarte Villavicencio <al222010329@gmail.com>
2024-01-18 22:41:00 -0600

    Django errors added

--- plagio/views.py
+++ plagio/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 #from duckduckgo_search import DDGS
+from duckduckgo_search.errors import HTTPError 
 from duckduckgo_search import DDGS
 from fuzzywuzzy import fuzz
 
@@ -7,23 +8,23 @@ from fuzzywuzzy import fuzz
 def similarity(a, b):
     return fuzz.partial_ratio(a, b)
 
-
 def search_view(request):
-    query = request.GET.get('q', '')  # Obtén el término de búsqueda de los parámetros GET
+    query = request.GET.get('q', '')  # Get the search term from the GET parameters
 
     results = []
     similar_count = 0
 
-    with DDGS() as ddg:
-        for r in ddg.text(query):
-            similarity_score = similarity(query, r.get('body', ''))
-            if similarity_score >= 60:  # Puedes ajustar este valor según tus necesidades
-                similar_count += 1
-            results.append({'result': r, 'similarity_score': similarity_score})
+    try:
+        with DDGS() as ddg:
+            for r in ddg.text(query):
+                similarity_score = similarity(query, r.get('body', ''))
+                if similarity_score >= 60:
+                    similar_count += 1
+                results.append({'result': r, 'similarity_score': similarity_score})
+    
+    except HTTPError as e:
+        # Handle HTTP errors from DuckDuckGo API
+        error_message = f"HTTP Error from DuckDuckGo API: {e}"
+        return JsonResponse({'error': error_message}, status=500)
 
-    response_data = {
-        'results': results,
-        'similar_count': similar_count
-    }
-
-    return JsonResponse(response_data, safe=False)
+     return JsonResponse(response_data, safe=False)
