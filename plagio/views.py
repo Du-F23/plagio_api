ca0a60c3e776eea9a72bcaa6875f77ff184685ce
Fernando Duarte Villavicencio <al222010329@gmail.com>
2024-06-05 17:37:22 -0600

    Resolve conflicts with duckduckgo search api

--- plagio/views.py
+++ plagio/views.py
@@ -1,12 +1,11 @@
 from django.http import JsonResponse
-#from duckduckgo_search import DDGS
-from duckduckgo_search.errors import HTTPError 
 from duckduckgo_search import DDGS
 from fuzzywuzzy import fuzz
 
 
 def similarity(a, b):
     return fuzz.partial_ratio(a, b)
+
 
 def search_view(request):
     query = request.GET.get('q', '')  # Get the search term from the GET parameters
@@ -15,16 +14,19 @@ def search_view(request):
     similar_count = 0
 
     try:
+        if not query:
+            return JsonResponse({'error': 'No query provided, please send'}, status=400)
+
         with DDGS() as ddg:
             for r in ddg.text(query):
                 similarity_score = similarity(query, r.get('body', ''))
                 if similarity_score >= 60:
                     similar_count += 1
                 results.append({'result': r, 'similarity_score': similarity_score})
-    
-    except HTTPError as e:
+
+        return JsonResponse({'results': results, 'similar_count': similar_count})
+
+    except Exception as e:
         # Handle HTTP errors from DuckDuckGo API
         error_message = f"HTTP Error from DuckDuckGo API: {e}"
         return JsonResponse({'error': error_message}, status=500)
-
-     return JsonResponse(response_data, safe=False)
