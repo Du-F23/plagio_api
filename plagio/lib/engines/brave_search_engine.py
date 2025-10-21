c14ef2642c04fa9b9322078d26f947aa7ffd68ab
du4rt3 <fernandoduarte1v@gmail.com>
2025-10-20 20:52:54 -0600

    (fix): update to crimp text and return data

--- plagio/lib/engines/brave_search_engine.py
+++ plagio/lib/engines/brave_search_engine.py
@@ -12,12 +12,15 @@ logger = logging.getLogger(__name__)
 
 class BraveSearchEngine(CommonEngineStrategy):
     def __init__(self):
+        logger.info(f'Initializing BraveSearchEngine with Key: {settings.BRAVE_API_KEY}')
         self.engine = Brave(settings.BRAVE_API_KEY)
 
     def search(self, query: str, num_resultados: int = 10) -> List[Tuple[CommonResultsEngine, int]]:
         try:
             logger.info(f'Searching for {query} on Brave')
-            search_results = self.engine.search(q=query, count=num_resultados)
+            query = query.replace("\n", " ").strip()
+
+            search_results = self.engine.search(q=query[:400], count=num_resultados)
 
             return BraveSearchEngine.format_result(search_results.web_results, query)
         except Exception as e:
